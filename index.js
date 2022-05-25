2a6b8235ff3bda16ed2d3e62208d1f3826a2e04d
Ayush2020012016 <ayush.2020012016@tulas.edu.in>
2022-05-25 14:21:08 +0530

    commited

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ router.get("/api/get",controllers.get);
 
 app.use(router);
 
-app.use(express.static(path.join(__dirname, "/client")));
+// app.use(express.static(path.join(__dirname, "/client")));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
-  });
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+//   });
 
 
 
